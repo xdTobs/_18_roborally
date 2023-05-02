5689174ce54376d76ab5419a5e5cd4acb8d3ab47
Frederik Rolsted <rolle474@gmail.com>
2023-05-02 18:57:09 +0200

    Now adds ConveyorBelt pictures from a relative file path

--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/ConveyorBeltView.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/ConveyorBeltView.java
@@ -1,5 +1,8 @@
 package dk.dtu.compute.se.pisd.roborally.view;
 
+import dk.dtu.compute.se.pisd.roborally.model.ConveyorBelt;
+import dk.dtu.compute.se.pisd.roborally.model.FastConveyorBelt;
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -9,14 +12,25 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+
+import static dk.dtu.compute.se.pisd.roborally.model.Heading.*;
+
 public class ConveyorBeltView extends SpaceView {
-    public ConveyorBeltView(@NotNull Space space) {
-        super(space);
+    public ConveyorBeltView(@NotNull ConveyorBelt conveyorBelt) {
+        super(conveyorBelt);
 
-
-        ImageView greenArrow = new ImageView(new Image("C:\\Users\\rolle\\IdeaProjects\\_18_roborally\\Board_Element_Pictures\\Green_Arrow.PNG"));
+        String filePath = new File("Board_Element_Pictures/Green_Arrow.PNG").getAbsolutePath();
+        ImageView greenArrow = new ImageView(new Image(filePath));
         greenArrow.setFitHeight(SPACE_HEIGHT);
         greenArrow.setFitWidth(SPACE_WIDTH - 20);
+        if (conveyorBelt.getHeading() == EAST) {
+            greenArrow.setRotate(90);
+        } else if (conveyorBelt.getHeading() == SOUTH) {
+            greenArrow.setRotate(180);
+        } else if (conveyorBelt.getHeading() == WEST) {
+            greenArrow.setRotate(270);
+        }
         this.getChildren().add(greenArrow);
     }
 }
--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/FastConveyorBeltView.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/FastConveyorBeltView.java
@@ -1,16 +1,32 @@
 package dk.dtu.compute.se.pisd.roborally.view;
 
+import dk.dtu.compute.se.pisd.roborally.model.ConveyorBelt;
+import dk.dtu.compute.se.pisd.roborally.model.FastConveyorBelt;
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import org.jetbrains.annotations.NotNull;
 
-public class FastConveyorBeltView extends SpaceView{
-    public FastConveyorBeltView(@NotNull Space space) {
-        super(space);
-        ImageView blueArrow = new ImageView(new Image("C:\\Users\\rolle\\IdeaProjects\\_18_roborally\\Board_Element_Pictures\\Blue_Arrow.PNG"));
+import java.io.File;
+
+import static dk.dtu.compute.se.pisd.roborally.model.Heading.*;
+
+public class FastConveyorBeltView extends SpaceView {
+    public FastConveyorBeltView(@NotNull ConveyorBelt conveyorBelt) {
+        super(conveyorBelt);
+
+        String filePath = new File("Board_Element_Pictures/Blue_Arrow.PNG").getAbsolutePath();
+        ImageView blueArrow = new ImageView(new Image(filePath));
         blueArrow.setFitHeight(SPACE_HEIGHT);
         blueArrow.setFitWidth(SPACE_WIDTH - 20);
+        if (conveyorBelt.getHeading() == EAST) {
+            blueArrow.setRotate(90);
+        } else if (conveyorBelt.getHeading() == SOUTH) {
+            blueArrow.setRotate(180);
+        } else if (conveyorBelt.getHeading() == WEST) {
+            blueArrow.setRotate(270);
+        }
         this.getChildren().add(blueArrow);
     }
 }
