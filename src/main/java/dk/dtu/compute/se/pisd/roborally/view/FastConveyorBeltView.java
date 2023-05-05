f6a1e9ea8c6ad3d560abda253d6702149cd6113f
henrik <henrik.zenkert@gmail.com>
2023-05-05 15:49:47 +0200

    small cleanup

--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/FastConveyorBeltView.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/FastConveyorBeltView.java
@@ -5,39 +5,19 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
-import static dk.dtu.compute.se.pisd.roborally.model.Heading.*;
-
 public class FastConveyorBeltView extends SpaceView {
     public FastConveyorBeltView(@NotNull ConveyorBelt conveyorBelt) {
         super(conveyorBelt);
-        //String filePath = new File("Board_Element_Pictures/Blue_Arrow.png").toURI().toString();
-
-        if (conveyorBelt.getHeading() == EAST) {
-            String filePath = new File("Board_Element_Pictures/blue-arrow-east.png").toURI().toString();
-            appendStyle("-fx-background-image: url('" + filePath + "');" +
-                    "-fx-background-size: 100% 100%;" +
-                    "-fx-background-repeat: no-repeat;" +
-                    "-fx-background-position: center;");
-        } else if (conveyorBelt.getHeading() == SOUTH) {
-            String filePath = new File("Board_Element_Pictures/blue-arrow-south.png").toURI().toString();
-            appendStyle("-fx-background-image: url('" + filePath + "');" +
-                    "-fx-background-size: 100% 100%;" +
-                    "-fx-background-repeat: no-repeat;" +
-                    "-fx-background-position: center;");
-        } else if (conveyorBelt.getHeading() == WEST) {
-            String filePath = new File("Board_Element_Pictures/blue-arrow-west.png").toURI().toString();
-            appendStyle("-fx-background-image: url('" + filePath + "');" +
-                    "-fx-background-size: 100% 100%;" +
-                    "-fx-background-repeat: no-repeat;" +
-                    "-fx-background-position: center;");
-        } else if (conveyorBelt.getHeading() == NORTH) {
-            String filePath = new File("Board_Element_Pictures/blue-arrow-north.png").toURI().toString();
-            appendStyle("-fx-background-image: url('" + filePath + "');" +
-                    "-fx-background-size: 100% 100%;" +
-                    "-fx-background-repeat: no-repeat;" +
-                    "-fx-background-position: center;");
-        }
-
-
+        String filename = switch (conveyorBelt.getHeading()) {
+            case NORTH -> "blue-arrow-north.png";
+            case EAST -> "blue-arrow-east.png";
+            case SOUTH -> "blue-arrow-south.png";
+            case WEST -> "blue-arrow-west.png";
+        };
+        String imageFile = new File("Board_Element_Pictures/" + filename).toURI().toString();
+        appendStyle("-fx-background-image: url('" + imageFile + "');" +
+                "-fx-background-size: 100% 100%;" +
+                "-fx-background-repeat: no-repeat;" +
+                "-fx-background-position: center;");
     }
 }
