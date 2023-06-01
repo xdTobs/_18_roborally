38f551bb82afb0f7249bb2f94e09a9ad03dc10c5
xdTobs <70943428+xdTobs@users.noreply.github.com>
2023-06-01 10:21:39 +0200

    saving changes

--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/PlayerTemplate.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/PlayerTemplate.java
@@ -2,6 +2,7 @@ package dk.dtu.compute.se.pisd.roborally.fileaccess.model;
 
 import dk.dtu.compute.se.pisd.roborally.model.CommandCardField;
 import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
 import static dk.dtu.compute.se.pisd.roborally.model.Heading.SOUTH;
@@ -15,8 +16,8 @@ public class PlayerTemplate {
     public CommandCardFieldTemplate[] registerSlots;
     public CommandCardFieldTemplate[] availableCardSlots;
 
-    public PlayerTemplate(int x, int y, String name, String color, int checkpointCounter, Heading heading, CommandCardFieldTemplate[] registerSlots, CommandCardFieldTemplate[] availableCardSlots) {
-        this.x = x;
+    public PlayerTemplate(Player p) {
+        this.x = p.;
         this.y = y;
         this.name = name;
         this.color = color;
--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/SpaceTemplate.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/SpaceTemplate.java
@@ -2,15 +2,25 @@ package dk.dtu.compute.se.pisd.roborally.fileaccess.model;
 
 import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
 import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SpaceTemplate {
+    public SpaceTemplate(Space space){
+        this.playerOnField = space.getPlayer();
+        //TODO make get PlayerTemplate instead
+        this.actions = space.getActions();
+        this.y = space.y;
+        this.x = space.x;
+        this.walls = space.getWalls();
+        //TODO figure out set or walls
 
+    }
     public List<Heading> walls = new ArrayList<>();
     public List<FieldAction> actions = new ArrayList<>();
-
+    public PlayerTemplate playerOnField;
     public int x;
     public int y;
 
