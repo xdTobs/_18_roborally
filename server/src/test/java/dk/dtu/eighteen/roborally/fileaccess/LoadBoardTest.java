698ab7646e5e8e6aa5b0581e6a9560246fb88151
Frederik Rolsted <rolle474@gmail.com>
2023-06-11 18:51:54 +0200

    Wrote unittest savingAndLoadingGame, that test loadNewGameFromFile(), saveBoard(), and loadSavedGameFromFile() methods from the LoadBoard class.

--- server/src/test/java/dk/dtu/eighteen/roborally/fileaccess/LoadBoardTest.java
+++ server/src/test/java/dk/dtu/eighteen/roborally/fileaccess/LoadBoardTest.java
@@ -1,10 +1,15 @@
 package dk.dtu.eighteen.roborally.fileaccess;
 
+import dk.dtu.eighteen.roborally.model.Board;
+import dk.dtu.eighteen.roborally.model.Player;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import java.io.File;
 import java.io.IOException;
 
+
 class LoadBoardTest {
+
 
     @Test
     void loadBoardFromJSONString() {
@@ -172,6 +177,48 @@ class LoadBoardTest {
 //        BoardTemplate boardTemplate = BoardTemplate.createBoardTemplate(jsonObject);
 //        assertEquals(3, boardTemplate.width);
 //        assertEquals(3, boardTemplate.height);
+
+    }
+
+
+    @Test
+    /**
+     * This tests the loadNewGameFromFile(), saveBoard(), and loadSavedGameFromFile() methods from
+     * the LoadBoard class.
+     * @Author Frederik Rolsted, s224299@dtu.dk
+     */
+    void savingAndLoadingGame() throws IOException {
+        Board board = LoadBoard.loadNewGameFromFile("DIZZY_HIGHWAY.json");
+        board.createAddPlayerToEmptySpace("PURPLE", "player1");
+
+        try {
+            LoadBoard.saveBoard(board, "savedTestBoard.json");
+            Board board2 = LoadBoard.loadSavedGameFromFile("savedTestBoard.json");
+            Player playerFromOrigBoard = board.getPlayer(0);
+            Player playerFromSavedBoard = board2.getPlayer(0);
+
+            // Assert relevant attributes of the boards
+            Assertions.assertEquals(board.height, board2.height);
+            Assertions.assertEquals(board.width, board2.width);
+            Assertions.assertEquals(board.getPhase(), board2.getPhase());
+            Assertions.assertEquals(board.getStep(), board2.getStep());
+            Assertions.assertEquals(board.isStepMode(), board2.isStepMode());
+
+            // Check if spacetype is the same on both boards with a wall
+            Assertions.assertEquals(board.getSpace(1, 2).getWalls(), board2.getSpace(1, 2).getWalls());
+
+            // Assert relevant attributes of the players
+            Assertions.assertEquals(playerFromOrigBoard.getName(), playerFromSavedBoard.getName());
+            Assertions.assertEquals(playerFromOrigBoard.getColor(), playerFromSavedBoard.getColor());
+            Assertions.assertEquals(playerFromOrigBoard.getSpace().x, playerFromSavedBoard.getSpace().x);
+            Assertions.assertEquals(playerFromOrigBoard.getSpace().y, playerFromSavedBoard.getSpace().y);
+        } finally {
+            File savedBoardFile = new File("src/main/resources/savedBoards/savedTestBoard.json");
+            if (savedBoardFile.exists()) {
+                savedBoardFile.delete();
+            }
+
+        }
 
     }
 }
