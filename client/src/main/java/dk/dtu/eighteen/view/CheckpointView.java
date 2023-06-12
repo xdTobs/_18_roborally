da54c8d1465d2ea74ddff370ff9096fe8c4316ed
Frederik Rolsted <rolle474@gmail.com>
2023-06-12 17:26:41 +0200

    Players dont rotate anymore on checkpoints

--- client/src/main/java/dk/dtu/eighteen/view/CheckpointView.java
+++ client/src/main/java/dk/dtu/eighteen/view/CheckpointView.java
@@ -15,32 +15,28 @@ public class CheckpointView extends SpaceView {
             appendStyle("-fx-background-image: url('" + filePath + "');" +
                     "-fx-background-size: 100% 100%;" +
                     "-fx-background-repeat: no-repeat;" +
-                    "-fx-background-position: center;" +
-                    "-fx-rotate: 270;");
+                    "-fx-background-position: center;");
         }
         if (checkpointNr == 2) {
             String filePath = new File("client/src/main/resources/imgs/Checkpoint2.png").toURI().toString();
             appendStyle("-fx-background-image: url('" + filePath + "');" +
                     "-fx-background-size: 100% 100%;" +
                     "-fx-background-repeat: no-repeat;" +
-                    "-fx-background-position: center;" +
-                    "-fx-rotate: 270;");
+                    "-fx-background-position: center;");
         }
         if (checkpointNr == 3) {
             String filePath = new File("client/src/main/resources/imgs/Checkpoint3.png").toURI().toString();
             appendStyle("-fx-background-image: url('" + filePath + "');" +
                     "-fx-background-size: 100% 100%;" +
                     "-fx-background-repeat: no-repeat;" +
-                    "-fx-background-position: center;" +
-                    "-fx-rotate: 270;");
+                    "-fx-background-position: center;");
         }
         if (checkpointNr == 4) {
             String filePath = new File("client/src/main/resources/imgs/Checkpoint4.png").toURI().toString();
             appendStyle("-fx-background-image: url('" + filePath + "');" +
                     "-fx-background-size: 100% 100%;" +
                     "-fx-background-repeat: no-repeat;" +
-                    "-fx-background-position: center;" +
-                    "-fx-rotate: 270;");
+                    "-fx-background-position: center;");
         }
 
     }
