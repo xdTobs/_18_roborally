96752a06c8000f6a6c2a71247d1137225f70f1f5
henrik <henrik.zenkert@gmail.com>
2023-06-12 09:16:56 +0200

    script for running game on rest api

--- server/src/test/run-game.sh
+++ server/src/test/run-game.sh
@@ -5,7 +5,7 @@ function prettyJson() {
 }
 
 # Create game
-gameId=$(curl -s --location 'localhost:8080/game' \
+gameId=$(curl -s -X POST --location 'localhost:8080/game' \
   --header 'Content-Type: application/json' \
   --data '{
   "playerCapacity": 2,
@@ -13,15 +13,15 @@ gameId=$(curl -s --location 'localhost:8080/game' \
   "playerName": "player-1"
 }
 ')
+echo ''
+echo "gameId: $gameId"
+echo ''
+
 responseJoin=$(curl -s --location "localhost:8080/game/$gameId" \
   --header 'roborally-player-name: player-2')
 
 responsePoll=$(curl -s --location "localhost:8080/game/$gameId" \
   --header 'roborally-player-name: player-1')
-
-echo ''
-echo "gameId: $gameId"
-echo ''
 
 echo 'join'
 echo "$responseJoin" | jq '.board.players[] | {name, x ,y, heading, firstCard: .playableCards[0].card.command}'
@@ -31,23 +31,32 @@ echo 'poll'
 echo "$responsePoll" | jq '.board.players[] | {name, x ,y, heading, firstCard: .playableCards[0].card.command}'
 echo ''
 
-
-moveP1=$(curl -s --location "localhost:8080/game/$gameId/moves" \
-  --header 'Content-Type: application/json' \
-  --header 'roborally-player-name: player-1' \
-  --data '[ 'empty', 'empty', 'empty', 'empty', 'empty' ]' 
+moveP1=$(
+  curl -s --location "localhost:8080/game/$gameId/moves" \
+    --header 'Content-Type: application/json' \
+    --header 'roborally-player-name: player-1' \
+    --data '[ 'empty', 'empty', 'option_left_right', 'empty', 'empty' ]'
 )
 
-moveP2=$(curl -s --location "localhost:8080/game/$gameId/moves" \
-  --header 'Content-Type: application/json' \
-  --header 'roborally-player-name: player-2' \
-  --data '[ 'empty', 'empty', 'empty', 'empty', 'empty' ]' 
+moveP2=$(
+  curl -s --location "localhost:8080/game/$gameId/moves" \
+    --header 'Content-Type: application/json' \
+    --header 'roborally-player-name: player-2' \
+    --data '[ 'empty', 'empty', 'empty', 'empty', 'empty' ]'
 )
 echo $moveP1
 echo $moveP2
 
+interactiveMoveResponse=$(
+  curl -s -X POST --location "localhost:8080/game/$gameId/moves/left" \
+    --header 'roborally-player-name: player-1'
+)
+
+echo "$interactiveMoveResponse"
+#
 responseAfterMove=$(curl -s --location "localhost:8080/game/$gameId" \
   --header 'roborally-player-name: player-1')
 
 echo "$responseAfterMove" | jq '.board.players[] | {name, x ,y, heading, firstCard: .playableCards[0].card.command}'
+#echo "$responseAfterMove"
 
